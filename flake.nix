e7b34bb2f48304b301485068ed15f6c77d621fa2
MolassesLover <mail@molasses.love>
2026-07-02 17:04:47 +0200

    Added all officially supported Nix platforms to Nix Flake

--- flake.nix
+++ flake.nix
@@ -9,8 +9,11 @@
     { self, nixpkgs }:
     let
       supportedSystems = [
-        "x86_64-linux"
         "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
       ];
 
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
